d87972463f9383e93df25fcae9d0542432b8357f
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-07-04 18:04:57 -0300

    Updated the file and a script so they're up to date

--- get_money_simulator/scripts/leaderstats.lua
+++ get_money_simulator/scripts/leaderstats.lua
@@ -1,19 +1,33 @@
 game.Players.PlayerAdded:Connect(function(player)
 	wait(5)
+	local player_name = player.Name
+	local dss = game:GetService("DataStoreService")
 	local leaderstats = Instance.new("Folder")
 	leaderstats.Name = "leaderstats"
 	leaderstats.Parent = player	
-	local moneys = Instance.new("IntValue")
-	moneys.Name = "Money"
-	moneys.Value = "0"
-	moneys.Parent = leaderstats
+	local money = Instance.new("IntValue")
+	money.Name = "Money"
+	money.Value = 0
+	money.Parent = leaderstats
 	local damage = Instance.new("IntValue")
 	damage.Name = "Damage"
-	damage.Value = _G.damage
+	damage.Value = 0
 	damage.Parent = leaderstats
+	local money_ts = dss:GetDataStore("Money")
+	local damage_ts = dss:GetDataStore("Damage")
+	if money_ts:GetAsync(player_name) then
+		money.Value = money_ts:GetAsync(player_name)
+	end
+	if damage_ts:GetAsync(player_name) then	
+		damage.Value = damage_ts:GetAsync(player_name)
+	end
 	while true do
-		wait(2)
+		wait(5)
 		damage.Value = _G.damage
+		local cv_money = money.Value
+		local cv_damage = damage.Value
+		money_ts:SetAsync(player_name, cv_money)
+		damage_ts:SetAsync(player_name, cv_damage)
 	end
 end)
-	
+
